b32b2650135f90cf1b6291bfc76ba4b43fc68808
Getinet Asimare <getinetasimare@gmail.com>
2025-07-11 16:04:45 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +20,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+          <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+
       <div className="App">
             {/* Render the WelcomeMessage component here.
                 This is how you use a custom React component within another component's JSX. */}
