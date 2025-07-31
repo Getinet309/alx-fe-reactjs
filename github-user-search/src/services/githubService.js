62a03b4214a1aebe3f3c6c1baadc851b428de753
Getinet309 <getinetasimare@gmail.com>
2025-07-31 20:02:25 +0300

    new search enjine

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,16 @@
+
+import axios from 'axios';
+
+const GITHUB_API_BASE_URL = 'https://api.github.com/users/';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_BASE_URL}${username}`);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      throw new Error('User not found');
+    }
+    throw error;
+  }
+};
