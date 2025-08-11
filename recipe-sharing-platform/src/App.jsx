e7f6def034b1e66c61abd40dbd34274f366e031e
Getinet309 <getinetasimare@gmail.com>
2025-08-11 09:23:23 +0300

    routing adding

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,17 @@
 // src/App.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import './index.css'; // Make sure to import your main CSS file for Tailwind to work
+import RecipeDetail from './components/RecipeDetail';
+import './index.css'; // Make sure to import your main CSS file for Tailwind
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
