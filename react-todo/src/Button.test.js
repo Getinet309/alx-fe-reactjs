4dfa2eb087de1aa58b996a8b68e3f6ae15c56768
Getinet309 <getinetasimare@gmail.com>
2025-08-21 19:14:42 +0300

    updated

--- react-todo/src/Button.test.js
+++ react-todo/src/Button.test.js
@@ -1,14 +1,16 @@
 // src/Button.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'; // This gives you matchers like toBeInTheDocument
-import Button from './Button'; // Import the component you want to test
+import '@testing-library/jest-dom';
+import Button from './Button';
 
-// Your test code goes here
 test('renders a button with the correct text', () => {
   render(<Button>Click Me</Button>);
-
+  
+  // This line will print the rendered HTML to your console
+  screen.debug(); 
+  
   const buttonElement = screen.getByText(/Click Me/i);
-
+  
   expect(buttonElement).toBeInTheDocument();
 });
